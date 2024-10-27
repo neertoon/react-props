f603857c78f19563dbed8d7f67b6d8ef20635fe4
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-10-27 17:43:52 +0100

    Nowy komponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+          <TimerChallenge title="Easy" targetTime={1} />
+          <TimerChallenge title="Lekko spocony" targetTime={5} />
+          <TimerChallenge title="Cięzki" targetTime={10} />
+          <TimerChallenge title="Dla miszczów" targetTime={15} />
+      </div>
     </>
   );
 }
