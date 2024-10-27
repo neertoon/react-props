09727adfe9ae4b0083a848a0e3a1f33280eb1ce5
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-10-27 18:40:20 +0100

    Pokazując modal resetowało nam wartość i modal i tak otwierał się ze złą wartością, dlatego resetowanie przenosmy do modala

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,10 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
     let thisDialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formatedRemainingTime = (remainingTime / 1000).toFixed(2);
 
     useImperativeHandle(ref, () => {
         return {
@@ -13,10 +16,10 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
 
     return (
         <dialog ref={thisDialog} className="result-modal">
-            <h2>Your {result} lost/won</h2>
+            <h2>You {userLost ? 'lost' : 'won'}</h2>
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
-            <p>You stopped the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog">
+            <p>You stopped the timer with <strong>{formatedRemainingTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
