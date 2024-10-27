70304b7fadabe5f8a6057fbf38844a2360a4b0c9
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-10-27 18:22:56 +0100

    !! useImperativeHandle tworzenie API dla komponentów

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,18 @@
-import { forwardRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+    let thisDialog = useRef();
 
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                thisDialog.current.showModal();
+            }
+        };
+    });
 
     return (
-        <dialog ref={ref} className="result-modal">
+        <dialog ref={thisDialog} className="result-modal">
             <h2>Your {result} lost/won</h2>
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>X seconds left.</strong></p>
