18a8f386a909cfcccf538785a95deab7f51dc208
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-10-27 17:49:32 +0100

    Mamy możliwośc odpalania timera ale jak go zatrzymać skoro nie jest i nie powinien być state?

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,19 +1,34 @@
+import { useState } from 'react';
+
 export default function TimerChallenge({ title, targetTime }) {
+    const [ timerExpired, setTimerExpired ] = useState(false);
+    const [ timerStarted, setTimerStarted ] = useState(false);
+    function handleStart() {
+        setTimeout(() => {
+            setTimerExpired(true)
+        }, 1000 * targetTime);
+
+        setTimerStarted(true);
+    }
+
+    function handleStop() {
+
+    }
+
     return (
     <section className="challenge">
         <h2>{title}</h2>
+        {timerExpired && <p>You lost!</p>}
         <p className="challenge-time">
             {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
         <p>
-            <button>
-                Start Challenge
-                Stop Challenge
+            <button onClick={handleStart}>
+                {timerStarted ? 'Stop Challenge' : 'Start Challenge'}
             </button>
         </p>
-        <p className="active">
-            Time is runnigng
-            Timer inactive
+        <p className={timerStarted ? 'active' : ''}>
+            {timerStarted ? 'Time is running' : 'Timer inactive'}
         </p>
     </section>
     );
